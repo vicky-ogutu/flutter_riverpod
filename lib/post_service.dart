5e418d032b04269379311f6e33e42c1808883bbd
vicky-ogutu <vikogutu77@gmail.com>
2025-03-28 09:20:59 +0300

    post_service

--- lib/post_service.dart
+++ lib/post_service.dart
@@ -1,10 +1,26 @@
-// import 'pq';
-//
-// class PostService{
-//
-//   Future<String> submitPost(String title, String body)async{
-//
-//     final response = await http.post()
-//   }
-//
-// }
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class PostService{
+
+  Future<String> submitPost(String title, String body) async {
+
+    Future<String> postData(String title, String body) async {
+      final response = await http.post(
+          Uri.parse("https://jsonplaceholder.typicode.com/posts"),
+          headers: {"Content-Type": "application/json"},
+          body: jsonEncode({"title": title, "body": body}),
+          );
+
+      if(response.statusCode==201){
+        return "success";
+      }else{
+       throw Exception("cannot post");
+      }
+    }
+return "success";
+  }
+
+}
