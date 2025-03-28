74933d25b56fb1c5657830099b0f3f64e0d09994
vicky-ogutu <vikogutu77@gmail.com>
2025-03-28 09:48:10 +0300

    future_post_provider

--- lib/post_provider.dart
+++ lib/post_provider.dart
@@ -3,7 +3,16 @@
 import 'package:flutter_riverpod/post_service.dart';
 import 'package:riverpod/riverpod.dart';
 
+//post service provider
 final PostServiceProvider =Provider<PostService>((ref) {
   return PostService();
+
+});
+
+//futureprovider for Api call
+
+final submitfutureprovider = FutureProviderFamily<String, Map<String, String>>((ref, data) {
+  final postService =ref.read(PostServiceProvider);
+  return postService.submitPost(data["title"]!, data["body"]!);
 
 });
